16f5a7ba2f30d007615ba5b13f9a90eeab291129
burikAV <AVburik84@gmail.com>
2020-03-06 14:00:34 +0300

    feat: add script algoritm )

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newbracketsConfig2 = bracketsConfig.map(function(num) {
+        return num.join('')
+    });
+
+    function replaceBrackets(string) {
+
+        console.log(string, newbracketsConfig2)
+        for (let i = newbracketsConfig2.length - 1; i >= 0; i--) {
+            if (string.includes(newbracketsConfig2[i])) {
+                string = string.replace(newbracketsConfig2[i], '');
+                return replaceBrackets(string);
+            } else if (string.length === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return replaceBrackets(str);
 }
