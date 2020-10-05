ea88d73f4172c0bd973e6b9d2d6319724aaf437d
burik84 <avburik84@gmail.com>
2020-10-05 13:01:11 +0300

    fix: refactor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-    let newbracketsConfig2 = bracketsConfig.map(function(num) {
-        return num.join('')
-    });
+    const newBracketsConfig = bracketsConfig.map((num)=> num.join(''));
 
     function replaceBrackets(string) {
-
-        console.log(string, newbracketsConfig2)
-        for (let i = newbracketsConfig2.length - 1; i >= 0; i--) {
-            if (string.includes(newbracketsConfig2[i])) {
-                string = string.replace(newbracketsConfig2[i], '');
+        // console.log(string, newBracketsConfig)
+        const lengthBracketsConfig=newBracketsConfig.length
+        for (let i = 0; i < lengthBracketsConfig; i++) {
+            if (string.includes(newBracketsConfig[i])) {
+                string = string.replace(newBracketsConfig[i], '');
                 return replaceBrackets(string);
             } else if (string.length === 0) {
                 return true;
